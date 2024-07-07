417d9956ca2af6fb47697e9c1a54845add101b04
josephthejoe <joseph.patenaude1@gmail.com>
2024-07-07 22:47:26 +0000

    built main an a handler

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func newRouter() *mux.Router {
+    r := mux.NewRouter()
+    r.HandleFunc("/hello", handler).Methods("GET")
+
+    staticFileDirectory := http.Dir("./web/")
+	staticFileHandler := http.StripPrefix("/web/", http.FileServer(staticFileDirectory))
+	r.PathPrefix("/web/").Handler(staticFileHandler).Methods("GET")
+
+    r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+
+    return r
+}
+func main() {
+    r := newRouter()
+    http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "hello, world")
+}
+
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/josephthejoe/webservertesting
 
 go 1.22.4
+
+require github.com/gorilla/mux v1.8.1 // indirect

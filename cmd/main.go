4e8f665d0c093caa67da5b8053b91f9eb7c8dac7
josephthejoe <joseph.patenaude1@gmail.com>
2024-07-11 00:44:10 +0000

    new structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,74 @@
 package main
 
 import (
-    "fmt"
+    "html/template"
     "net/http"
-
-    "github.com/jospehthejoe/webservertesting/internal/handlers"
+    "fmt"
     "github.com/gorilla/mux"
-
 )
 
-func newRouter() *mux.Router {
-    r := mux.NewRouter()
-    r.HandleFunc("/hello", handler).Methods("GET")
-
-    staticFileDirectory := http.Dir("./web/")
-	staticFileHandler := http.StripPrefix("/web/", http.FileServer(staticFileDirectory))
-	r.PathPrefix("/web/").Handler(staticFileHandler).Methods("GET")
-
-    r.HandleFunc("/bird", getBirdHandler).Methods("GET")
-	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
-
-    return r
+// Data structure for the greeting template
+type GreetingData struct {
+    Name string
 }
+
 func main() {
-    r := newRouter()
-    http.ListenAndServe(":8080", r)
+    // Create a new router
+    router := mux.NewRouter()
+
+    // Define a handler function for the home page
+    homeHandler := func(w http.ResponseWriter, r *http.Request) {
+        // Parse the HTML template
+        tmpl, err := template.ParseFiles("./web/templates/index.html")
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+
+        // Execute the template
+        err = tmpl.Execute(w, nil)
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+    }
+
+    // Define a handler function for the greeting page
+    greetHandler := func(w http.ResponseWriter, r *http.Request) {
+        // Get the "name" query parameter from the URL
+        name := r.URL.Query().Get("name")
+
+        // If the name is empty, set a default name
+        if name == "" {
+            name = "Guest"
+        }
+
+        // Create a GreetingData instance
+        data := GreetingData{Name: name}
+
+        // Parse the HTML template
+        tmpl, err := template.ParseFiles("./web/templates/greeting.html")
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+
+        // Execute the template with the data
+        err = tmpl.Execute(w, data)
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+    }
+
+    // Register the handlers for the home and greet pages
+    router.HandleFunc("/", homeHandler).Methods(http.MethodGet)
+    router.HandleFunc("/greet", greetHandler).Methods(http.MethodGet)
+
+    // Serve static files from the "static" directory
+    router.PathPrefix("./web/static/").Handler(http.StripPrefix("./web/static/", http.FileServer(http.Dir("static"))))
+
+    // Start the HTTP server on port 8080 using the router
+    fmt.Println("Server is listening on :8080...")
+    http.ListenAndServe(":8080", router)
 }
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello, world")
-}
-
-
